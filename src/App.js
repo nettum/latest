94c043bfc39311672993a2d5cbd636d0caef7ae8
Marius Nettum <nettum@gmail.com>
2021-08-26 13:53:40 +0200

    Add component to render api data (starting with discogs data)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Feed from './Feed';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Hello, my name is Marius</h1>
       </header>
+      <main>
+        <Feed type="discogs" title="Check out the latest music I bought" />
+      </main>
     </div>
   );
 }
---
+++ src/Feed.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+const Feed = (props) => {
+  const { type, title } = props;
+  const [ items, setItems ] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async (type) => {
+      const response = await fetch(`/api/${type}`);
+      const json = await response.json();
+      setItems(json);
+    }
+    fetchData(type);
+  }, [type]);
+
+  const renderItem = (item) => {
+    if (type === 'discogs') {
+      return (<li key={item.basic_information.id}>
+        <a href={`https://www.discogs.com/release/${item.basic_information.id}`}>
+          <img src={item.basic_information.thumb} alt={`${item.basic_information.title} album cover`} />
+          <h4>{item.basic_information.title}</h4>
+          <h5>{item.basic_information.artists[0].name}</h5>
+        </a>
+      </li>);
+    }
+
+    return null;
+
+  };
+
+  return (
+    <section>
+      <h3>{title}</h3>
+      {items && items.length > 0 && (
+        <ul>
+          {items.map(item => renderItem(item))}
+        </ul>
+      )}
+    </section>
+  );
+};
+
+export default Feed;
