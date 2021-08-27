19a5799af90e618b35d438ed698e33cd86ed97b1
Marius Nettum <nettum@gmail.com>
2021-08-27 12:53:20 +0200

    Add styled-components and test styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import Feed from './Feed';
+import styled, { createGlobalStyle } from 'styled-components';
 
 const App = () => {
+
+  const GlobalStyles = createGlobalStyle`
+    body {
+      padding: 0;
+      background: blue;
+    }
+    ul, ol {
+      list-style: none;
+    }
+  `;
+
+  const Wrapper = styled.div`
+    max-width: 1280px;
+    margin: 0 auto;
+  `;
+
+  const Header = styled.header`
+    background: red;
+    span {
+      color: yellow;
+    }
+  `;
+
+  const  Main = styled.main`
+    background: green;
+  `;
+
+  const MainHeader = styled.h1``;
+
+  const SubHeader = styled.h2``;
+
   return (
-    <div className="container">
-      <header>
-        <h1>Hello, my name is Marius</h1>
-      </header>
-      <main>
+    <Wrapper>
+      <GlobalStyles />
+      <Header>
+        <MainHeader>Hello, my name is <span>Marius</span></MainHeader>
+        <SubHeader>Here's what I've been up to lately</SubHeader>
+      </Header>
+      <Main>
         <Feed type="discogs" title="Check out the latest music I bought" />
         <Feed type="episodes" title="The latest tv-shows I saw" />
         <Feed type="movies" title="The latest movie I watched" />
-      </main>
-    </div>
+      </Main>
+    </Wrapper>
   );
 }
 
