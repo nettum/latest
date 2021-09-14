58007fc23968845328c7122dcd6e987a2d72eb87
Marius Nettum <nettum@gmail.com>
2021-09-15 01:52:46 +0200

    Add footer text about gig source

--- src/App.js
+++ src/App.js
@@ -85,7 +85,8 @@ const App = () => {
           <a href="https://www.discogs.com/developers" target="_blank" rel="noopener noreferrer">Discogs</a>,&nbsp;
           <a href="https://trakt.docs.apiary.io/" target="_blank" rel="noopener noreferrer">Trakt</a>,&nbsp;
           <a href="https://developers.themoviedb.org/" target="_blank" rel="noopener noreferrer">TMDB</a>,&nbsp;
-          <a href="https://developer.spotify.com/documentation/web-api/" target="_blank" rel="noopener noreferrer">Spotify</a>
+          <a href="https://developer.spotify.com/documentation/web-api/" target="_blank" rel="noopener noreferrer">Spotify</a>,&nbsp;
+          <a href="https://www.sanity.io/docs/datastore" target="_blank" rel="noopener noreferrer">Sanity</a>
         </p>
         <p>Hosting by <a href="https://vercel.com/" target="_blank" rel="noopener noreferrer">Vercel</a></p>
       </Footer>
