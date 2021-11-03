3ec789042814eecb8e6e0c9582ebab29ac5b0503
Marius Nettum <nettum@gmail.com>
2021-11-03 08:05:01 +0100

    Update footer with info about untappd API

--- src/App.js
+++ src/App.js
@@ -87,7 +87,8 @@ const App = () => {
           <a href="https://trakt.docs.apiary.io/" target="_blank" rel="noopener noreferrer">Trakt</a>,&nbsp;
           <a href="https://developers.themoviedb.org/" target="_blank" rel="noopener noreferrer">TMDB</a>,&nbsp;
           <a href="https://developer.spotify.com/documentation/web-api/" target="_blank" rel="noopener noreferrer">Spotify</a>,&nbsp;
-          <a href="https://www.sanity.io/docs/datastore" target="_blank" rel="noopener noreferrer">Sanity</a>
+          <a href="https://www.sanity.io/docs/datastore" target="_blank" rel="noopener noreferrer">Sanity</a>,&nbsp;
+          <a href="https://untappd.com/api/docs" target="_blank" rel="noopener noreferrer">Untappd</a>
         </p>
         <p>Hosting by <a href="https://vercel.com/" target="_blank" rel="noopener noreferrer">Vercel</a></p>
       </Footer>
